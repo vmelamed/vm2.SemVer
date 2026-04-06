ae5091ab31a6c5ac544a6f6f81346b992bb3b594
Val Melamed <valo@vtmelamed.com>
2026-04-06 19:41:12 -0400

    feat: add Newtonsoft.Json and System.Text.Json converters for SemVer serialization

---
+++ src/SemVer/Serialization/NsJson/SemVerNsConverter.cs
@@ -0,0 +1,75 @@
+using System;
+
+namespace vm2.SemVerSerialization.NsJson;
+
+using Newtonsoft.Json;
+
+/// <summary>
+/// Provides functionality to convert <see cref="vm2.SemVer"/> values to and from JSON format.
+/// Implements the Newtonsoft.Json.<see cref="JsonConverter{T}"/>".
+/// </summary>
+/// <remarks>This converter is used to serialize and deserialize <see cref="SemVer"/> values in JSON format. It
+/// ensures that <see cref="SemVer"/> instances are correctly represented as strings in JSON and parsed back into <see
+/// cref="SemVer"/> objects during deserialization.</remarks>
+public class SemVerNsConverter : JsonConverter
+{
+    /// <summary>
+    /// Determines whether the specified type can be converted to or from a <see cref="SemVer"/>.
+    /// </summary>
+    /// <param name="objectType">The type to evaluate for conversion compatibility.</param>
+    /// <returns>
+    /// <see langword="true"/> if the specified type is <see cref="SemVer"/> or <see cref="Nullable{SemVer}"/>; otherwise, <see langword="false"/>.
+    /// </returns>
+    public override bool CanConvert(Type objectType)
+        => objectType == typeof(SemVer) || objectType == typeof(SemVer?);
+
+    /// <summary>
+    /// Writes the JSON representation of the specified object using the provided <see cref="JsonWriter"/>.
+    /// </summary>
+    /// <param name="writer">The <see cref="JsonWriter"/> used to write the JSON output. Cannot be <c>null</c>.</param>
+    /// <param name="value">The object to serialize. Can be <c>null</c>, in which case a JSON null value is written.</param>
+    /// <param name="serializer">The <see cref="JsonSerializer"/> used to customize the serialization process. Cannot be <c>null</c>.</param>
+    public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+    {
+        if (value is SemVer semVer)
+        {
+            writer.WriteValue(semVer.ToString());
+            return;
+        }
+
+        // Debug.Assert(false, "This should never happen because JsonConvert.SerializeObject  should prevent it.");
+        throw new JsonWriterException($"Expected value to be of type {typeof(SemVer)} or null, but got {value?.GetType()}.");
+    }
+
+    /// <summary>
+    /// Reads JSON data from the specified <see cref="JsonReader"/> and converts it into an object of the specified
+    /// type.
+    /// </summary>
+    /// <param name="reader">The <see cref="JsonReader"/> to read JSON data from.</param>
+    /// <param name="objectType">The type of the object to deserialize the JSON data into.</param>
+    /// <param name="existingValue">An existing object to populate with the JSON data, or <c>null</c> to create a new object.</param>
+    /// <param name="serializer">The <see cref="JsonSerializer"/> used to deserialize the JSON data.</param>
+    /// <returns>The deserialized object, or <c>null</c> if the JSON data is empty or cannot be deserialized.</returns>
+    public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
+    {
+        try
+        {
+            if (reader.TokenType is JsonToken.Null ||
+                reader.Value is null ||
+                reader.Value.ToString() is null)
+                return null;
+
+            if (reader.TokenType is not JsonToken.String)
+            {
+                // Debug.Assert(false, "This should never happen because JsonConvert.DeserializeObject should prevent it.");
+                throw new JsonReaderException($"Expected token type to be {JsonToken.String} or {JsonToken.Null}, but got {reader.TokenType}.");
+            }
+
+            return SemVer.Parse(reader.Value.ToString()!);
+        }
+        catch (Exception ex) when (ex is not JsonReaderException)
+        {
+            throw new JsonReaderException("Could not parse SemVer value.", ex);
+        }
+    }
+}
